165663dee9769954367f610e7c23d72197c6d187
Upasna Langar <langarupasna.2@gmail.com>
2018-11-16 17:30:50 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string)
+{    return string.toUpperCase();
+    } 
+    
+    function whisper(string)
+    { 
+      return string.toLowerCase();
+    }
+    
+    function logShout(string)
+    { 
+      var upperstring = string.toUpperCase();
+      console.log(upperstring);
+    }
+  
+  function logWhisper(string)
+  {
+  
+    console.log(string.toLowerCase());
+    
+  }
+  
+  function sayHiToGrandma(string)
+  { var lowercase = string.toLowerCase();
+  
+  lowercase=== string.toLowerCase();
+    return()
+    
+  }
+  
+  
